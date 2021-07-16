2d8c1b9a56ad0a0c0a387fdc7394a1a71bf6fad1
ymj <2513667783@qq.com>
2021-07-16 20:01:32 +0800

    创建表结构以及添加测试用例

---
+++ imooc-coupon-service/coupon-template/src/test/java/com/imooc/coupon/TemplateApplicationTests.java
@@ -0,0 +1,22 @@
+package com.imooc.coupon;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Classname TemplateApplicationTests
+ * @Description 模板系统测试程序
+ * @Date 2021/7/16 19:22
+ * @Created by yemingjie
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TemplateApplicationTests {
+    @Test
+    public void contextLoad() {
+
+    }
+}
